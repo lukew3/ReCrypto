92ee42fe42cdcb422090d15f95a62d9d880160e0
lukew3 <lukew25073@gmail.com>
2020-01-03 21:39:20 -0500

    added database uri

--- recrypto/__init__.py
+++ recrypto/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, session, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 from flask_portfolio import routes

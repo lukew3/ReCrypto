312a9ea2d24dea5583f7e1ae4f1c5d83497d95f8
lukew3 <lukew25073@gmail.com>
2020-01-04 00:44:25 -0500

    ideas and setup for login

--- recrypto/__init__.py
+++ recrypto/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask, session, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from recrypto import routes
--- recrypto/routes.py
+++ recrypto/routes.py
@@ -1,12 +1,14 @@
 from flask import render_template, url_for, flash, redirect
-from recrypto import app
+from recrypto import app, db
+from recrypto.models import User
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/")
 @app.route("/landing")
 def landing():
     return render_template('landing.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 

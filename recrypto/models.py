c64abc9f72fd539d3a048580cbb0a77641f17597
lukew3 <lukew25073@gmail.com>
2020-01-04 03:38:52 -0500

    trying to get login to work

--- recrypto/models.py
+++ recrypto/models.py
@@ -3,8 +3,9 @@ from recrypto import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(11), unique=False, nullable=False)
-    profilePicture = db.Column(db.String(20), unique=False, nullable=False, default="default.jpg")
+    email = db.Column(db.String(30), unique=False, nullable=False)
+    password = db.Column(db.String(50), unique=False, nullable=False)
+    #profilePicture = db.Column(db.String(20), unique=False, nullable=False, default="default.jpg")
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.profilePicture}')"
+        return f"User('{self.username}', '{self.email}' )"
--- recrypto/routes.py
+++ recrypto/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from recrypto import app, db
 from recrypto.models import User
 from flask_login import login_user, current_user, logout_user, login_required
+from recrypto.forms import LoginForm
 
 username = ''
 password = ''
@@ -9,20 +10,31 @@ password = ''
 @app.route("/")
 @app.route("/landing")
 def landing():
-    #if current_user.is_logged_in:
-    #    return redirect(url_for('home'))
-    #else:
-    #    return render_template('landing.html')
-    return render_template('landing.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    else:
+        return render_template('landing.html')
+    #return render_template('landing.html')
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
-    username = request.form.get('username')
-    password = request.form.get('password')
-    user = User.query.filter_by(username).first()
-    if (username != ''):
-        return redirect(url_for(confirmlogin), username=username, password=password)
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm();
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and (user.password == form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('login unsuccessful')
+    return render_template('login.html', form=form)
+    #username = request.form.get('username')
+    #password = request.form.get('password')
+    #user = User.query.filter_by(username).first()
+    #if (username != ''):
+    #    return redirect(url_for(confirmlogin), username=username, password=password)
 
     #if user and (user.password == password):
     #    login_user(user, remember=)
